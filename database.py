a245d22fac90f69a43314949708d246106bcf43f
JackaDu <jackdu.academic@gmail.com>
2024-04-27 11:46:41 +0800

    add requirements

--- database.py
+++ database.py
@@ -16,9 +16,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # Load and display the contents of each CSV file to check
-csv_files = {
-    'loan': './test.csv',
-}
-for table_name, file_path in csv_files.items():
-    df = pd.read_csv(file_path)
-    df.to_sql(table_name, engine, if_exists='replace', index=False)
+# large_file = './public_up_to_150k_12_230930.csv'
+# small_file = './test.csv'
+# csv_files = {
+#     'loan': large_file,
+# }
+# for table_name, file_path in csv_files.items():
+#     df = pd.read_csv(file_path)
+#     df.to_sql(table_name, engine, if_exists='replace', index=False)
